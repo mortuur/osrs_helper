4b1c8437ae41d6634b4d23de0de335048cf5e766
bart_van_der_plas <bart.vanderplas@student.rocva.nl>
2024-05-29 09:09:53 +0200

    start of frond-end trade page

--- resources/views/components/trade/block.blade.php
+++ resources/views/components/trade/block.blade.php
@@ -1,3 +1,3 @@
-<div class="w-3/5 h-40 border border-solid border-black">
+<div class="w-1/4 h-40 border border-solid border-black mx-10">
     <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
 </div>
--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -1,7 +1,11 @@
 <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
 <x-app>
-    <div class="flex justify-center items-center mx-auto border-solid border-2 border-black h-fit mt-16 w-3/5 p-2">
+    <div class="flex justify-center items-center mx-auto border-solid border-2 border-black h-fit mt-16 w-4/5 p-2">
+        {{-- maak er foreach van op een later moment --}}
+        @for ($i = 0; $i < 3; $i++)
         <x-trade.block />
+            
+        @endfor
     </div>
         {{-- de line hier onder is voor later --}}
             {{-- {{dd($items)}} --}}

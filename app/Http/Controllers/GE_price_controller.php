11abe19df74f8c72c4b8a8d1a12037e03185a40f
bart_van_der_plas <bart.vanderplas@student.rocva.nl>
2024-06-10 10:09:08 +0200

    point of return monday

--- app/Http/Controllers/GE_price_controller.php
+++ app/Http/Controllers/GE_price_controller.php
@@ -6,5 +6,5 @@ use Illuminate\Http\Request;
 
 class GE_price_controller extends Controller
 {
-    //
+    
 }
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Http\Controllers\pullDataController
+
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::job(new Price_update())
+            ->name('check_uptime:laravel.com')
+            ->everyFiveMinutes()
+            ->onOneServer();
